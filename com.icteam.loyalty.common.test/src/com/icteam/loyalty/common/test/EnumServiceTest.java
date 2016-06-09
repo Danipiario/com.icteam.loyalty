5611143aaebdc4fa978006a907a987cdeb6eb3f1
Danipiario <daniele.pirola@gmail.com>
2016-06-09 17:36:55 +0200

    test brach

--- com.icteam.loyalty.common.test/src/com/icteam/loyalty/common/test/EnumServiceTest.java
+++ com.icteam.loyalty.common.test/src/com/icteam/loyalty/common/test/EnumServiceTest.java
@@ -18,31 +18,32 @@ import com.icteam.loyalty.common.api.interfaces.IEnum;
 
 public class EnumServiceTest {
 
-	private final BundleContext context = FrameworkUtil.getBundle(this.getClass()).getBundleContext();
+    private final BundleContext context = FrameworkUtil.getBundle(this.getClass()).getBundleContext();
 
-	<T> T getService(Class<T> clazz) throws InterruptedException {
-		final ServiceTracker<T, T> st = new ServiceTracker<>(context, clazz, null);
-		st.open();
-		return st.waitForService(1000);
-	}
+    <T> T getService(Class<T> clazz) throws InterruptedException {
+        final ServiceTracker<T, T> st = new ServiceTracker<>(context, clazz, null);
+        st.open();
+        return st.waitForService(1000);
+    }
 
-	/*
-	 *
-	 */
-	@Test
-	public void contextNotNull() throws Exception {
-		Assert.assertNotNull(context);
-	}
+    /*
+     *
+     */
+    @Test
+    public void contextNotNull() throws Exception {
+        Assert.assertNotNull(context);
 
-	@Test
-	public void serviceNotNull() throws InterruptedException {
-		Assert.assertNotNull(getService(EnumService.class));
-	}
+    }
 
-	@Test
-	public void valuesOfIAlfa() throws InterruptedException {
-		final List<? extends IEnum> values = getService(EnumService.class).values(IAlfa.class);
-		System.out.println(values);
-		Assert.assertNotNull(values);
-	}
+    @Test
+    public void serviceNotNull() throws InterruptedException {
+        Assert.assertNotNull(getService(EnumService.class));
+    }
+
+    @Test
+    public void valuesOfIAlfa() throws InterruptedException {
+        final List< ? extends IEnum> values = getService(EnumService.class).values(IAlfa.class);
+        System.out.println(values);
+        Assert.assertNotNull(values);
+    }
 }
